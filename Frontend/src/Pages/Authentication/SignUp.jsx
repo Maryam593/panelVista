0d88ab8fd561ecb8b9420fe8ec0558e1d66a82f4
Maryam saba <maryams91101@gmail.com>
2025-03-09 01:02:07 +0500

    update auth UI

--- Frontend/src/Pages/Authentication/SignUp.jsx
+++ Frontend/src/Pages/Authentication/SignUp.jsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useEffect,useRef } from "react";
 import { useForm } from "react-hook-form";
 import { signupSchema } from "./SignUpSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import gsap from "gsap";
 import InputField from "../../Components/InputField";
 import ButtonHandle from "../../Components/ButtonHandle";
 
 const SignUp = () => {
+    const formRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      formRef.current,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, duration: 3, ease: "power3.out" }
+    );
+  }, []);
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitted },
   } = useForm({
     resolver: zodResolver(signupSchema),
-    mode: "onSubmit", 
+    mode: "onSubmit",
   });
 
   const onSubmit = (data) => {
     console.log("Form Data:", data);
   };
+
+
   const fields = [
     { label: "First Name", name: "FullName.firstName", type: "text", placeholder: "John" },
     { label: "Last Name", name: "FullName.lastName", type: "text", placeholder: "Doe" },
@@ -27,7 +39,7 @@ const SignUp = () => {
 
   return (
     <div className="bg-gray-100 flex justify-center items-center h-screen">
-      <div className="bg-white p-6 rounded-lg shadow-md w-96">
+      <div className="bg-white p-6 rounded-lg shadow-md w-96 signup-container" ref={formRef}>
         <h2 className="text-2xl font-bold mb-4 text-center">Sign Up</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
           {fields.map(({ label, name, type, placeholder }) => (
@@ -39,13 +51,15 @@ const SignUp = () => {
               placeholder={placeholder}
               register={register}
               error={isSubmitted && errors?.[name.split(".")[0]]?.[name.split(".")[1]]}
+              className="input-field"
             />
           ))}
-          <ButtonHandle type="submit" text="Create an Account" />
+          <ButtonHandle type="submit" text="Create an Account" className="submit-btn" />
         </form>
       </div>
     </div>
   );
-};
+
+}
 
 export default SignUp;
