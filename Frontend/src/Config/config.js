5c51adaed8044fb67d47f787a483d3cf687826c2
Maryam saba <maryams91101@gmail.com>
2025-03-10 01:14:19 +0500

    routes updation

--- Frontend/src/Config/config.js
+++ Frontend/src/Config/config.js
@@ -1,12 +1,12 @@
 
 export const URLS = {
     AUTH :  {
-        LOGIN : "o/Auth/user/login",
-        SIGNUP : "o/Auth/user/sign-up"
+        LOGIN : "o/auth/user/login",
+        SIGNUP : "o/auth/user/sign-up"
     },
     HOME : "/",
     DASHBOARD : "/user/dashboard",
     PROFILE : "/user/profile",
-    DELETE : "user/delete/profile",
-    CHANGE_PASSWORD : "user/change-password"
+    DELETE : "/user/delete/profile",
+    CHANGE_PASSWORD : "/user/change-password"
 }
